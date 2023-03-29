be3f45b9b7bf19f4b3a0004f0b4bada032b75d57
Tamunokuro <tamunokrobluejack@gmail.com>
2023-03-29 22:34:52 +0100

    Fix linters

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,6 @@
 class FoodsController < ApplicationController
   before_action :set_food, only: %i[show edit update destroy]
-
-
+  
   # GET /foods or /foods.json
   def index
     @foods = current_user.foods
