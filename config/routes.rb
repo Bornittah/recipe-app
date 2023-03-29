5681d202eab0befceafbb8a3affbbf546be074b0
bornittah <bornittah@gmail.com>
2023-03-29 17:33:41 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
-  resources :users, only: [:index, :show]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index, :show] do
+    resources :foods, only: [:index, :show, :new]
+  end
 end
